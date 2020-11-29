89535015ec5e31bc0f052c6ed7959c5275c6ce4e
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-11-29 18:16:01 +0900

    read data from FIFO to PC done

---
+++ recordLoadSound.cpp
@@ -0,0 +1,122 @@
+#include "ftd2xx.h"
+#include <stdio.h>
+#include "stdlib.h"
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <signal.h> 
+#include <unistd.h>
+
+FT_HANDLE   ftHandle[4];
+FT_STATUS   ftStatus;
+DWORD       numDevs;
+DWORD		devIndex;
+int			devcnt, devlast;
+int         array_cnt;
+char        buf[256]; // -127 to 127
+DWORD       WriteNum, TransNum;
+DWORD       ReadNum;
+unsigned char bufc[128]; //保存できるのは0-255
+double a[1986560];
+double a_copy[1986560];
+int i, j, k, stop;
+unsigned char tmp_even, tmp_odd;
+
+using namespace std;
+
+void my_handler(int s){
+    printf("Caught signal %d\n",s);
+    buf[0] = 0x04;
+    ftStatus = FT_Write(ftHandle[devcnt], buf, WriteNum, &TransNum);
+    cout << "closing device no. " << devcnt << endl;
+    devcnt = 0;
+    ftStatus = FT_Close(ftHandle[devcnt]);  // USB Device CLOSE
+    exit(1); 
+
+}
+
+
+int main(){
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+
+    numDevs = 0;
+    ftStatus = FT_ListDevices(&numDevs,NULL,FT_LIST_NUMBER_ONLY);
+    cout << ftStatus << endl;
+    printf("%d\n", numDevs);
+    devlast = numDevs;
+    cout << "loop starting" << endl;
+	for(devcnt = 0;devcnt != devlast; devcnt++){
+        devIndex = (DWORD)devcnt;
+        ftStatus = FT_ListDevices((PVOID)devIndex, buf, FT_LIST_BY_INDEX|FT_OPEN_BY_SERIAL_NUMBER);
+        if (ftStatus==FT_OK) {
+            ftStatus = FT_OpenEx(buf, FT_OPEN_BY_SERIAL_NUMBER, &ftHandle[devcnt]);
+            if (ftStatus==FT_OK) {
+                cout << "Open ok" << endl;
+            }
+            devcnt=0;	// device #0		
+            TransNum = 0; WriteNum=1; 
+			buf[0]=0x02; //address counter clear command 0x02
+            ftStatus = FT_Write(ftHandle[devcnt],buf,WriteNum,&TransNum);
+            if (ftStatus==FT_OK) {
+                cout << "address clear successful" << endl;
+            }
+            TransNum = 0; WriteNum=1; 
+			buf[0]=0x04; //read initialization
+            ftStatus = FT_Write(ftHandle[devcnt],buf,WriteNum,&TransNum);
+            stop = 0;
+            if (ftStatus==FT_OK) {
+                cout << "print initialization successful" << endl;
+
+                while (1) {
+                    devcnt = 0;
+                    TransNum = 0; WriteNum = 1;
+                    buf[0] = 0x03;
+                    // write into memory
+                    cout << "start recording for 3 seconds" << endl;
+                    ftStatus = FT_Write(ftHandle[devcnt], buf, WriteNum, &TransNum);
+                    sleep(3);
+                    cout << "stopping and processing" << endl;
+                    buf[0] = 0x04;
+                    ftStatus = FT_Write(ftHandle[devcnt], buf, WriteNum, &TransNum);
+                    sleep(3);
+                    for (i=0;i<64;i++){
+                        cout << "set transfer len to 128" << endl;
+                        TransNum = 0; WriteNum=1; 
+			            buf[0]=0x08; // transfer len set to be 128
+                        ftStatus = FT_Write(ftHandle[devcnt],buf,WriteNum,&TransNum);
+                        for (j=0;j<128;j++) bufc[j]=buf[j];
+                        sleep(3);
+
+                        cout << "USB FIFO data load command" << endl;
+                        buf[0]=0x05; // USB FIFO data load command
+                        ftStatus = FT_Write(ftHandle[devcnt],buf,WriteNum,&TransNum);
+                        for (j=0;j<128;j++) bufc[j]=buf[j];
+                        sleep(3);
+
+                        cout << "Reading USB data" << endl;
+                        TransNum = 0; WriteNum=0; ReadNum=128; 
+                        ftStatus = FT_Read(ftHandle[devcnt],bufc,ReadNum,&TransNum);			   
+                        for (j=0;j<64;j++) {
+                            a[j+i*64]=bufc[2*j]+bufc[2*j+1]*256;
+                            cout << a[j+i*64] << endl;
+                        }
+                        sleep(3);
+                    }
+                }
+            }
+
+            cout << "closing" << endl;
+            ftStatus = FT_Close(ftHandle[devcnt]);  // USB Device CLOSE
+            break;
+        }
+    }
+
+    return 0;
+}
