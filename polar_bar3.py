eb92c57d38e4dcef444b2bf49eead29bca65e34e
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-12-10 13:41:31 +0900

    chart with title and color bar title

--- polar_bar3.py
+++ polar_bar3.py
@@ -11,15 +11,36 @@ rad = np.linspace(0, 10, m)
 a = np.linspace(0, 2 * np.pi, n)
 r, th = np.meshgrid(rad, a)
 
-z = np.random.uniform(-1, 1, (n,m))
+#z = np.random.uniform(-1, 1, (n,m))
+
+z = np.array([
+    [0.9, 0.8, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7,],
+    [0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
+    [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7],
+    [0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6],
+    [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4],
+    [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+    [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+    [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+    [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7],
+    [0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
+    [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9],
+    [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9],
+])
+
 print(z.shape)
 print(z)
 plt.subplot(projection="polar")
 
-plt.pcolormesh(th, r, z, cmap = 'inferno')
+plt.pcolormesh(th, r, z, cmap = 'YlOrRd')
 
-plt.plot(a, r, ls='none', color = 'k') 
+plt.plot(a, r, ls='none', color = 'k')
+plt.title("Power Distribution With Frequency In Radial Direction")
 plt.grid()
-plt.colorbar()
+plt.colorbar(label="Scaled Power")
+"""
+arr1 = plt.arrow(0, 0.5, 0, 15, alpha = 0.5, width = 0.015,
+                 edgecolor = 'black', facecolor = 'green', lw = 2, zorder = 5)
+"""
 plt.savefig('a.png')
 plt.show()
