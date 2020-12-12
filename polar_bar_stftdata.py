bf9a616880f8f4ae2daedf70237d77cd1e574135
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-12-12 16:06:18 +0900

    change cmap to jet

--- polar_bar_stftdata.py
+++ polar_bar_stftdata.py
@@ -3,9 +3,11 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pandas as pd
 
-z_raw = pd.concat([pd.read_csv(f"stftdata-1210/{str(i*10)}.csv")["0"].loc[10:20, ] for i in range(36)], axis=1).values
+z_raw = pd.concat([pd.read_csv(f"fftdata-1210/{str(i*10)}_fft.csv")["0"] for i in range(36)], axis=1).values
 print(type(z_raw))
 print("shape of raw z", z_raw.shape)
+
+z_raw = np.log10(z_raw)
 
 maximum = z_raw.max()
 minimum = z_raw.min()
@@ -19,7 +21,7 @@ ax = Axes3D(fig)
 
 n = z.shape[0] # z.shape[0]
 m = z.shape[1] # z.shape[1]
-rad = np.linspace(0, maximum, m)
+rad = np.linspace(0, 10**maximum // 1000, m)
 print("rad shape", rad.shape)
 a = np.linspace(0, 2 * np.pi, n)
 r, th = np.meshgrid(rad, a)
@@ -30,7 +32,7 @@ print(z.shape)
 print(z)
 plt.subplot(projection="polar")
 
-plt.pcolormesh(th, r, z, cmap = 'YlOrRd')
+plt.pcolormesh(th, r, z, cmap = 'jet')
 
 plt.plot(a, r, ls='none', color = 'k')
 plt.title("Power Distribution With Frequency In Radial Direction")
